10347d91e99ee1cf7a08155712a171458889253f
Vipul Gaurav <30434974+vgaurav3011@users.noreply.github.com>
2020-09-08 16:36:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"iamitachiuchiha007@gmail.com\"\n\
+email = \"vgaurav3011@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
